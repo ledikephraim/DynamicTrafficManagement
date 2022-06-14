24a78588586603cf6b9cd20f29b7aa9e7aa79a56
Ledike Mahlare <ledike.mahlare@bcx.co.za>
2022-06-14 20:16:52 +0200

    Broker initial commit

---
+++ DynamicTrafficManager/Broker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Broker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
