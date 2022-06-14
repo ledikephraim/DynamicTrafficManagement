f80f83c181c1537f4f210f403783bed713f16113
Ledike Mahlare <ledike.mahlare@bcx.co.za>
2022-06-14 20:21:19 +0200

    TrafficLightUnit Client initial commit

---
+++ DynamicTrafficManager/TrafficLightUnitClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrafficLightUnitClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
